099f017addf9907b3a1c08e2690dbe203173c8fa
redolf250 <redolfkendrick@gmail.com>
2023-04-18 11:18:50 +0000

    refactored

--- backend/packages/misc.py
+++ backend/packages/misc.py
@@ -1,3 +1,3 @@
-from scan_devices.camera import ActiveCameras
 from alert.alert_dialog import *
-from program_dept.program_dept import *
+from database.database import *
+from scan_devices.camera import ActiveCameras
