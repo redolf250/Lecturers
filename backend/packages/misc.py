99e605bc509ba2dac44ccbdc28692fb016fac317
redolf250 <redolfkendrick@gmail.com>
2023-04-18 11:26:49 +0000

    modified

--- backend/packages/misc.py
+++ backend/packages/misc.py
@@ -1,3 +1,2 @@
-from alert.alert_dialog import *
 from database.database import *
-from scan_devices.camera import ActiveCameras
+from alert.alert_dialog import *
--- backend/packages/ui_files.py
+++ backend/packages/ui_files.py
@@ -2,4 +2,4 @@ from mail.ui_mail import Ui_Mail
 from launcher.ui_launcher import Ui_MainWindow
 from dashboard.ui_dashoboard import Ui_dashboard
 from alert.ui_alert_dialog import Ui_AlertDialog
-from program_dept.ui_program_dept import Ui_Database
+from database.ui_program_dept import Ui_Database
